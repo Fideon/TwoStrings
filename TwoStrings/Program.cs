25a66cc61e0a53a7a028f787e7ca6bc908a0c493
Fidel Salazar <fidelsf91@gmail.com>
2020-11-16 14:32:30 -0600

    Solved with better performance

--- TwoStrings/Program.cs
+++ TwoStrings/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TwoStrings
 {
     class Program
     {
+        static string twoStrings(string s1, string s2)
+        {
+            List<char> list = s1.ToCharArray().ToList();
+            list = list.Distinct().ToList();
+
+            foreach (char c in list)
+            {
+                if (s2.ToList().Remove(c)) return "YES";
+            }
+            return "NO";
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            twoStrings("a", "cat");
         }
     }
 }
